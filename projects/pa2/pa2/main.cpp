2f1b76d0a49e037d3592acd2ecd57d232dcac5e5
Jay Revolinsky <54556864+jayrevolinsky@users.noreply.github.com>
2019-09-16 14:57:42 -0700

    Update main.cpp

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -1,8 +1,19 @@
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main(void)
+int main() 
 {
-	cout << "hello, world!";
-	return 0;
+    string readfile;
+    ifstream myfile;
+    myfile.open("../../../Desktop/bin/2019-fall-cs112/projects/pa2/pa2/tinypix.ppm");
+    
+    while(myfile.good() == true)
+    {
+            getline(myfile, readfile); 
+            cout << readfile << endl;
+    }
+    return 0;
 }
